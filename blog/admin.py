0142e2847e38808daaf0f8d906ab0be6c862e755
Max Donald <maxaraye18@gmail.com>
2024-02-26 02:26:09 +0100

    Mise en ligne premier site

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import CreateBlog,Comment
+
+# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('title','intro','slug','date_added')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display =('body','email','date_added')
+
+admin.site.register(CreateBlog,BlogAdmin)
+admin.site.register(Comment,CommentAdmin)
+
---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,redirect
+from django.views.generic import ListView
+from .models import CreateBlog,Comment
+from .forms import BlogForm
+
+# Create your views here.
+class List(ListView):
+    template_name = 'blog/index.html'
+    queryset = CreateBlog.objects.all()
+    paginate_by = 2
+
+def detailView(request,slug):
+    post=CreateBlog.objects.get(slug=slug)
+    comments=post.comments.all()
+    if request.method=='POST':
+        form=BlogForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            form.instance.post=post
+            form.save()
+            return redirect('detailView',slug=post.slug)
+    else:
+        form=BlogForm
+    content={
+        'article':post,
+        'comments':comments,
+        'form':form,
+    }
+    return render(request,'blog/update.html',content)
